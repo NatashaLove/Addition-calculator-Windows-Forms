35ce2865a3b77eb917ba8509f8a768e85d17c1db
Natasha S Love <debora@inbox.ru>
2018-11-29 19:26:34 -0600

    added buttons

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,263 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtResolved = new System.Windows.Forms.TextBox();
+            this.chkNewForm = new System.Windows.Forms.CheckBox();
+            this.grpColor = new System.Windows.Forms.GroupBox();
+            this.radGreen = new System.Windows.Forms.RadioButton();
+            this.radBlue = new System.Windows.Forms.RadioButton();
+            this.radDefault = new System.Windows.Forms.RadioButton();
+            this.txtErrors = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.grpColor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(42, 24);
+            this.fileToolStripMenuItem.Text = "&file";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.clearToolStripMenuItem.Text = "&clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(113, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.exitToolStripMenuItem.Text = "e&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.helpToolStripMenuItem.Text = "help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(123, 26);
+            this.aboutToolStripMenuItem.Text = "about";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNumber1.Location = new System.Drawing.Point(257, 32);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(155, 22);
+            this.txtNumber1.TabIndex = 1;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(257, 60);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(155, 22);
+            this.txtNumber2.TabIndex = 2;
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Font = new System.Drawing.Font("Mistral", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber1.Location = new System.Drawing.Point(119, 28);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(132, 29);
+            this.lblNumber1.TabIndex = 3;
+            this.lblNumber1.Text = "Enter a number";
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.Font = new System.Drawing.Font("Mistral", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber2.Location = new System.Drawing.Point(119, 57);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(132, 29);
+            this.lblNumber2.TabIndex = 4;
+            this.lblNumber2.Text = "Enter a number";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(124, 88);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(127, 36);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "Add(+)";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(124, 128);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(127, 36);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtResolved
+            // 
+            this.txtResolved.BackColor = System.Drawing.SystemColors.Control;
+            this.txtResolved.Enabled = false;
+            this.txtResolved.Location = new System.Drawing.Point(257, 135);
+            this.txtResolved.Name = "txtResolved";
+            this.txtResolved.Size = new System.Drawing.Size(155, 22);
+            this.txtResolved.TabIndex = 7;
+            // 
+            // chkNewForm
+            // 
+            this.chkNewForm.AutoSize = true;
+            this.chkNewForm.Location = new System.Drawing.Point(257, 97);
+            this.chkNewForm.Name = "chkNewForm";
+            this.chkNewForm.Size = new System.Drawing.Size(131, 21);
+            this.chkNewForm.TabIndex = 8;
+            this.chkNewForm.Text = "open new form?";
+            this.chkNewForm.UseVisualStyleBackColor = true;
+            // 
+            // grpColor
+            // 
+            this.grpColor.Controls.Add(this.radGreen);
+            this.grpColor.Controls.Add(this.radBlue);
+            this.grpColor.Controls.Add(this.radDefault);
+            this.grpColor.Location = new System.Drawing.Point(124, 170);
+            this.grpColor.Name = "grpColor";
+            this.grpColor.Size = new System.Drawing.Size(288, 100);
+            this.grpColor.TabIndex = 9;
+            this.grpColor.TabStop = false;
+            this.grpColor.Text = "Error Text Color";
+            // 
+            // radGreen
+            // 
+            this.radGreen.AutoSize = true;
+            this.radGreen.Location = new System.Drawing.Point(190, 64);
+            this.radGreen.Name = "radGreen";
+            this.radGreen.Size = new System.Drawing.Size(66, 21);
+            this.radGreen.TabIndex = 2;
+            this.radGreen.Text = "green";
+            this.radGreen.UseVisualStyleBackColor = true;
+            // 
+            // radBlue
+            // 
+            this.radBlue.AutoSize = true;
+            this.radBlue.Location = new System.Drawing.Point(108, 64);
+            this.radBlue.Name = "radBlue";
+            this.radBlue.Size = new System.Drawing.Size(56, 21);
+            this.radBlue.TabIndex = 1;
+            this.radBlue.Text = "blue";
+            this.radBlue.UseVisualStyleBackColor = true;
+            // 
+            // radDefault
+            // 
+            this.radDefault.AutoSize = true;
+            this.radDefault.Checked = true;
+            this.radDefault.Location = new System.Drawing.Point(6, 64);
+            this.radDefault.Name = "radDefault";
+            this.radDefault.Size = new System.Drawing.Size(72, 21);
+            this.radDefault.TabIndex = 0;
+            this.radDefault.TabStop = true;
+            this.radDefault.Text = "default";
+            this.radDefault.UseVisualStyleBackColor = true;
+            // 
+            // txtErrors
+            // 
+            this.txtErrors.BackColor = System.Drawing.SystemColors.Control;
+            this.txtErrors.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtErrors.Enabled = false;
+            this.txtErrors.Location = new System.Drawing.Point(124, 293);
+            this.txtErrors.Name = "txtErrors";
+            this.txtErrors.Size = new System.Drawing.Size(288, 98);
+            this.txtErrors.TabIndex = 10;
+            this.txtErrors.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtErrors);
+            this.Controls.Add(this.grpColor);
+            this.Controls.Add(this.chkNewForm);
+            this.Controls.Add(this.txtResolved);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblNumber2);
+            this.Controls.Add(this.lblNumber1);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.grpColor.ResumeLayout(false);
+            this.grpColor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtResolved;
+        private System.Windows.Forms.CheckBox chkNewForm;
+        private System.Windows.Forms.GroupBox grpColor;
+        private System.Windows.Forms.RadioButton radGreen;
+        private System.Windows.Forms.RadioButton radBlue;
+        private System.Windows.Forms.RadioButton radDefault;
+        private System.Windows.Forms.RichTextBox txtErrors;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,5 +15,40 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void clearFields()
+        {
+            txtNumber1.Text = "";
+            txtNumber2.Text = "";
+            txtResolved.Text = "";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //before clearing - a confirmation message first:
+            //if user clicks - yes
+            if (MessageBox.Show("Are you sure you want to clear?", "Confirm", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                clearFields();
+            }
+            else
+            {
+                MessageBox.Show("I will not clear the fields.");
+            }
+        }
+
+        
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clearFields();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+      
     }
 }
